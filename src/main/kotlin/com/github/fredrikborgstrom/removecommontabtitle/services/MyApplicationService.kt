681da629923710c0bb20e64ed08709ca7ed49fa0
GitHub Action <action@github.com>
2021-10-10 10:56:06 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/fredrikborgstrom/removecommontabtitle/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fredrikborgstrom.removecommontabtitle.services
+
+import com.github.fredrikborgstrom.removecommontabtitle.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/fredrikborgstrom/removecommontabtitle/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fredrikborgstrom/removecommontabtitle/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.fredrikborgstrom.removecommontabtitle.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fredrikborgstrom.removecommontabtitle.MyBundle
 
 class MyProjectService(project: Project) {
 
